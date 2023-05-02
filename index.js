e6293d6b6d3f3c2c9226bb67a6881ba89214b7d8
23Mubasshir <23mubasshir@gmail.com>
2023-05-03 00:10:25 +0600

    Images Added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
+const chefCart = require('./data/chefdata.json');
+const cors = require('cors');
 
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Our site is running')
+})
+app.get('/chefCart', (req, res) => {
+  res.send(chefCart);
 })
 
 app.listen(port, () => {
